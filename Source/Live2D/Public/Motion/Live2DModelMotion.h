d6574f03f49ade14c9a8aa6de42bb4e35310622a
Stefan Krismann <stefan@krismann.at>
2022-06-19 18:49:43 +0200

    on importing model3 file, also import motion files as well

--- Source/Live2D/Public/Motion/Live2DModelMotion.h
+++ Source/Live2D/Public/Motion/Live2DModelMotion.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
+#include "Live2DMocModel.h"
 #include "Live2DModelMotionCurve.h"
 
 #include "Live2DModelMotion.generated.h"
@@ -15,8 +16,13 @@ class LIVE2D_API ULive2DModelMotion : public UObject
 	GENERATED_BODY()
 public:
 	bool Init(const FMotion3FileData& Motion3Data);
+	void SetModel(ULive2DMocModel* InModel) { Model = InModel;}
+	ULive2DMocModel* GetModel() const { return Model; }
 
 protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	ULive2DMocModel* Model;
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	float Duration;
 	
--- Source/Live2DEditor/Private/Model3JsonImportFactory.cpp
+++ Source/Live2DEditor/Private/Model3JsonImportFactory.cpp
@@ -8,8 +8,10 @@
 #include "Misc/MessageDialog.h"
 #include "Json.h"
 #include "Live2DEditorUtils.h"
+#include "Motion3JsonImportFactory.h"
 #include "Factories/Texture2dFactoryNew.h"
 #include "Factories/TextureFactory.h"
+#include "Motion/Live2DModelMotion.h"
 
 UModel3JsonImportFactory::UModel3JsonImportFactory()
 	:Super()
@@ -34,13 +36,36 @@ UObject* UModel3JsonImportFactory::FactoryCreateText(UClass* InClass, UObject* I
 	
 	if (MocModel->Init(Path / Model3Data.Moc))
 	{
-		UTextureFactory* Factory = NewObject<UTextureFactory>();
-		for (const auto& TextureName: Model3Data.Textures)
 		{
-			const FName TextureAssetName = *TextureName.Replace(TEXT("/"), TEXT("_")).Replace(TEXT("."), TEXT("_"));
-			UTexture2D* NewTexture = Cast<UTexture2D>(Factory->FactoryCreateFile(UTexture2D::StaticClass(), InParent, TextureAssetName, Flags, Path / TextureName, 0, Warn,bOutOperationCanceled));
-			MocModel->Textures.Add(NewTexture);
+			UTextureFactory* Factory = NewObject<UTextureFactory>();
+			for (const auto& TextureName: Model3Data.Textures)
+			{
+				const FName TextureAssetName = *TextureName.Replace(TEXT("/"), TEXT("_")).Replace(TEXT("."), TEXT("_"));
+				UTexture2D* NewTexture = Cast<UTexture2D>(Factory->FactoryCreateFile(UTexture2D::StaticClass(), InParent, TextureAssetName, Flags, Path / TextureName, 0, Warn,bOutOperationCanceled));
+				MocModel->Textures.Add(NewTexture);
+			}
 		}
+
+		{
+			UMotion3JsonImportFactory* Factory = NewObject<UMotion3JsonImportFactory>();
+			for (const auto& MotionData: Model3Data.Motions)
+			{
+				for (const FModel3MotionFileData& MotionFile : MotionData.Value.Motions)
+				{
+					const FName MotionAssetName = *MotionFile.File.Replace(TEXT("/"), TEXT("_")).Replace(TEXT(".json"), TEXT("")).Replace(TEXT("."), TEXT("_"));
+					const FString MotionFileName = MotionFile.File.Replace(TEXT(".json"), TEXT(""));
+					ULive2DModelMotion* NewModelMotion = Cast<ULive2DModelMotion>(Factory->FactoryCreateFile(UTexture2D::StaticClass(), InParent, MotionAssetName, Flags, Path / MotionFileName, 0, Warn,bOutOperationCanceled));
+
+					if (!NewModelMotion)
+					{
+						continue;
+					}
+
+					NewModelMotion->SetModel(MocModel);
+				}
+			}
+		}
+		
 		
 		return MocModel;
 	}
